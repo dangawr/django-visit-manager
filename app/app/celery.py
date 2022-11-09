1582a8e0d1bef42b0e07342d1ce5082f24a8db0e
sloniupck <sloniupython@gmail.com>
2022-11-09 16:48:04 +0100

    Crontab scheduler

--- app/app/celery.py
+++ app/app/celery.py
@@ -12,7 +12,9 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.conf.beat_schedule = {
     "every day between 6 AM & 18 PM": {
         "task": "xxx_execute_xx_task",  # <---- Name of task
-        "schedule": 5.0,
+        "schedule": crontab(hour=15,
+                            minute=47,
+                            )
     }
 }
 
