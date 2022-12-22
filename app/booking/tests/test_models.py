4f283564676f44d2a01ae0dd9bce1587bc1e771d
sloniupck <sloniupython@gmail.com>
2022-12-22 21:30:25 +0100

    test_models fix

--- app/booking/tests/test_models.py
+++ app/booking/tests/test_models.py
@@ -1,14 +1,23 @@
 from django.test import TestCase
 from booking.models import Client
+from django.contrib.auth import get_user_model
 
 
 class ModelsTestCase(TestCase):
 
+    def setUp(self) -> None:
+        user_details = {
+            'username': 'Test Name',
+            'password': 'test-user-password123'
+        }
+        self.user = get_user_model().objects.create_user(**user_details)
+
     def test_phone_number_field(self):
         client_data = {
             "first_name": "Test",
-            "second_name": "Name",
-            "phone_number": "+48600000000"
+            "last_name": "Name",
+            "phone_number": "+48600000000",
+            "user": self.user,
         }
         client = Client.objects.create(**client_data)
 
